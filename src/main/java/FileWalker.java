9510aed9b9a4efd114722423f14cbc793c330e0a
Evgeniya Katunina <evgeniya-katunina@yandex.ru>
2019-03-20 21:40:31 +0300

    Add check for package existence.

--- src/main/java/FileWalker.java
+++ src/main/java/FileWalker.java
@@ -1,6 +1,7 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import visitors.UnusedVariableVisitor;
+import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
@@ -10,14 +11,30 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileWalker extends SimpleFileVisitor<Path> {
 
+    private boolean isPackagePresent;
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        isPackagePresent = false;
         if (!isJava(file)) {
             return FileVisitResult.CONTINUE;
         }
         CompilationUnit compilationUnit = StaticJavaParser.parse(file);
-        UnusedVariableVisitor unusedVariableVisitor = new UnusedVariableVisitor();
-        unusedVariableVisitor.visit(compilationUnit, null);
+        System.out.println(String.format("Analyzing file: %s", file));
+        new VoidVisitorAdapter() {
+            @Override
+            public void visit(PackageDeclaration n, Object arg) {
+                isPackagePresent = true;
+            }
+        }.visit(compilationUnit, null);
+        boolean warningsDetected = false;
+        if (!isPackagePresent) {
+            warningsDetected = true;
+            System.out.println("[WARNING] No package declaration.");
+        }
+        if (!warningsDetected) {
+            System.out.println("No warnings have been detected.");
+        }
         return FileVisitResult.CONTINUE;
     }
 
